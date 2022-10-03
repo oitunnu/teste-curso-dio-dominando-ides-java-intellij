7dd202c64c3658cfa0c271fc3d827ff52bd2351d
Itunnu <itunnu.yussuf@gmail.com>
2022-10-03 18:14:03 -0300

    first commit

---
+++ src/com/dio/ExemploPrograma.java
@@ -0,0 +1,14 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class ExemploPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 4;
+        System.out.println("Yolo World! " + (a+b));*/
+    }
+}
