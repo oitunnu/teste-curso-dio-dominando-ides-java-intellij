0ab8915afcfe6654024bbd700e41c41ba2439e09
Itunnu <itunnu.yussuf@gmail.com>
2022-10-04 22:32:13 -0300

    creating class Calculadora

---
+++ src/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,43 @@
+package com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        double a, b;
+
+        System.out.println("Digite o primeiro valor: ");
+        a = scan.nextInt();
+        System.out.println("Digite o segundo valor: ");
+        b = scan.nextInt();
+
+        double somar = somar(a, b);
+        double subtrair = subtrair(a, b);
+        double multiplicar = multiplicar(a, b);
+        double dividir = dividir(a, b);
+
+        System.out.println("somar: " + somar);
+        System.out.println("subtrair : " + subtrair);
+        System.out.println("multiplicar: " + multiplicar);
+        System.out.println("dividir: " + dividir);
+
+    }
+
+    public static double somar(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtrair(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public static double dividir(double a, double b) {
+        return a / b;
+    }
+}
